b3bf5954de89baf5a22c63ce037c191df2b049ec
Abhinav Kumar <abhinku7@cisco.com>
2023-09-17 22:14:53 +0530

    initial commit

---
+++ src/main/java/com/abhinav/abhinav_backend_starter/AbhinavBackendStarterApplication.java
@@ -0,0 +1,14 @@
+package com.abhinav.abhinav_backend_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhinavBackendStarterApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(AbhinavBackendStarterApplication.class, args);
+//        System.out.println("Abhinav");
+
+    }
+
+}
---
+++ src/test/java/com/abhinav/abhinav_backend_starter/AbhinavBackendStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.abhinav_backend_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhinavBackendStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
